6fea46a326f0917b58b9638cda1a068836821e1a
RMP1Games <tynitskyivlad@gmail.com>
2025-10-06 20:43:14 +0300

    Final variant

---
+++ lesson2_17_09_25/lesson2_17_09_25/Migrations/20251006172839_ResultProblem.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lesson2_17_09_25.Migrations
+{
+    /// <inheritdoc />
+    public partial class ResultProblem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "TEXT",
+                oldDefaultValue: new DateTimeOffset(new DateTime(2025, 10, 6, 16, 39, 55, 965, DateTimeKind.Unspecified).AddTicks(7643), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Test",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "TEXT",
+                oldDefaultValue: new DateTimeOffset(new DateTime(2025, 10, 6, 16, 39, 55, 979, DateTimeKind.Unspecified).AddTicks(5764), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StartedAt",
+                table: "Attempt",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "TEXT",
+                oldDefaultValue: new DateTimeOffset(new DateTime(2025, 10, 6, 16, 39, 55, 983, DateTimeKind.Unspecified).AddTicks(6931), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2025, 10, 6, 16, 39, 55, 965, DateTimeKind.Unspecified).AddTicks(7643), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "TEXT",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Test",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2025, 10, 6, 16, 39, 55, 979, DateTimeKind.Unspecified).AddTicks(5764), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "TEXT",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "StartedAt",
+                table: "Attempt",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2025, 10, 6, 16, 39, 55, 983, DateTimeKind.Unspecified).AddTicks(6931), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+        }
+    }
+}
